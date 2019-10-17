02f0d7ff31f27d015ba99e39975eb383c9f87598
Ji Myoung Ha <webmaster@null0xff.com>
2019-10-17 12:51:33 +0900

    add Graph class

--- src/w06_p2p.java
+++ src/w06_p2p.java
@@ -1,82 +1,88 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class w06_p2p {
 
 	public static void main(String[] args) {
-		Graph graph = new Graph();
-		System.out.printf("%s", graph);
-	}
-
-}
-
-class Graph {
-
-	private Set<Node> nodeSet;
-
-	public Graph() {
-		this.init();
-	}
-
-	private void init() {
-		// Initialize Variable
+		// Initialize
 		Scanner inputStream = new Scanner(System.in);
-		this.nodeSet = new TreeSet<Node>();
-
-		// Input
-		int n = inputStream.nextInt();
-		int m = inputStream.nextInt();
-		String edgeStart, edgeEnd;
+		// Create Graph
+		Graph graph = new Graph();
+		int n, m;
+		n = inputStream.nextInt();
+		m = inputStream.nextInt();
 		for (int i = 0; i < n; i++)
-			this.nodeSet.add(new Node(inputStream.next()));
+			graph.addNode(inputStream.next());
+		String edgeStart, edgeEnd;
 		for (int i = 0; i < m; i++) {
 			edgeStart = inputStream.next();
 			edgeEnd = inputStream.next();
-			if (!findNode(edgeStart).getEdge().contains(findNode(edgeEnd))) {
-				findNode(edgeStart).getEdge().add(findNode(edgeEnd));
-				findNode(edgeEnd).getEdge().add(findNode(edgeStart));
-			}
+			graph.addEdge(edgeStart, edgeEnd);
 		}
+
+		// Sort Graph
+		graph.sort();
+
+		// Output
+		System.out.printf("%s", graph);
 
 		// Finalize
 		inputStream.close();
 	}
 
-	public Node findNode(Object value) {
-		Iterator<Node> nodeIterator = this.nodeSet.iterator();
-		Node nodePointer;
-		while (nodeIterator.hasNext()) {
-			nodePointer = nodeIterator.next();
-			if (nodePointer.getValue() == value)
-				return nodePointer;
+	private static class Graph {
+		private HashMap<String, List<String>> graphMap;
+
+		public Graph() {
+			this.graphMap = new HashMap<String, List<String>>();
 		}
-		return null;
+
+		public boolean addNode(String node) {
+			if (this.graphMap.containsKey(node))
+				return false;
+			this.graphMap.put(node, new ArrayList<String>());
+			return true;
+		}
+
+		public boolean addEdge(String nodeFront, String nodeEnd) {
+			this.addNode(nodeFront);
+			this.addNode(nodeEnd);
+			if (this.graphMap.get(nodeFront).contains(nodeEnd))
+				return false;
+			this.graphMap.get(nodeFront).add(nodeEnd);
+			this.graphMap.get(nodeEnd).add(nodeFront);
+			return true;
+		}
+
+		public void sort() {
+			this.graphMap.forEach((k, v) -> {
+				Collections.sort((List<String>) v);
+			});
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			Set<String> cache = new HashSet<String>();
+			this.graphMap.forEach((k, v) -> {
+				cache.add(k);
+				builder.append(String.format("%s: ", k));
+				v.forEach(e -> {
+					builder.append(String.format("%s", e));
+					if (v.indexOf(e) < v.size() - 1)
+						builder.append(", ");
+				});
+				if (graphMap.size() != cache.size())
+					builder.append("\n");
+			});
+			return builder.toString();
+		}
+
 	}
 
-}
-
-class Node implements Comparable<Node> {
-	private Object value;
-	private Set<Node> edge;
-
-	public Node(Object value) {
-		this.value = value;
-		this.edge = new HashSet<Node>();
-	}
-
-	public Object getValue() {
-		return this.value;
-	}
-
-	public Set<Node> getEdge() {
-		return this.edge;
-	}
-
-	@Override
-	public int compareTo(Node o) {
-		return ((String) this.getValue()).compareTo((String) o.getValue());
-	}
 }
