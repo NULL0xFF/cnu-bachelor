bcd50b895ca02002c8b815e0abb3627359183410
Ji Myoung Ha <webmaster@null0xff.com>
2019-10-18 13:46:48 +0900

    implements shared_mut

---
+++ week07/smart_obj/smart_ptr/shared_mut.cpp
@@ -0,0 +1,77 @@
+//
+// Created by 김혁진 on 14/10/2019.
+//
+
+#include "shared_mut.h"
+
+namespace ptr
+{
+shared_mut::shared_mut()
+{
+    _mgr = new mgr();
+}
+
+shared_mut::shared_mut(Object *_obj)
+{
+    _mgr = new mgr(_obj);
+}
+
+shared_mut::~shared_mut()
+{
+    release();
+}
+void shared_mut::release()
+{
+    _mgr->count = _mgr->count - 1;
+    if (_mgr->count == 0)
+        _mgr->~mgr();
+}
+Object *shared_mut::get() const
+{
+    return _mgr->ptr;
+}
+int shared_mut::count()
+{
+    return _mgr->count;
+}
+void shared_mut::increase()
+{
+    _mgr->count = _mgr->count + 1;
+}
+
+shared_mut shared_mut::operator+(const shared_mut &shared)
+{
+    Object obj(shared_mut::get()->get() + shared.get()->get());
+    shared_mut::shared_mut result(&obj);
+    return result;
+}
+shared_mut shared_mut::operator-(const shared_mut &shared)
+{
+    Object obj(shared_mut::get()->get() - shared.get()->get());
+    shared_mut::shared_mut result(&obj);
+    return result;
+}
+shared_mut shared_mut::operator*(const shared_mut &shared)
+{
+    Object obj(shared_mut::get()->get() * shared.get()->get());
+    shared_mut::shared_mut result(&obj);
+    return result;
+}
+shared_mut shared_mut::operator/(const shared_mut &shared)
+{
+    Object obj(shared_mut::get()->get() / shared.get()->get());
+    shared_mut::shared_mut result(&obj);
+    return result;
+}
+
+Object *shared_mut::operator->()
+{
+    return _mgr->ptr;
+}
+shared_mut &shared_mut::operator=(const shared_mut &r)
+{
+    shared_mut::release();
+    _mgr = r._mgr;
+    shared_mut::increase();
+}
+} // end of namespace ptr
