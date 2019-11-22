645e8777b09d11f82924c94e29df5cc0dd8c1b4f
Ji Myoung Ha <webmaster@null0xff.com>
2019-11-22 15:04:13 +0900

    update week12 assignment_2 base

--- week12/assignment_2/json_dict.cpp
+++ week12/assignment_2/json_dict.cpp
@@ -2,39 +2,9 @@
 
 // PUBLIC
 json_dict::json_dict() {}
-json_object *json_dict::parse(const char *input, int length)
+json_object *json_object::operator[](json_object *key)
 {
-    // TODO
-}
-json_object::_type json_dict::type() { return this->DICT; }
-std::string json_dict::to_string()
-{
-    // TODO
 }
 // PRIVATE
-void json_dict::put(json_object *key, json_object *val)
-{
-    int index = this->find(key);
-    if (index == -1)
-    {
-        this->v.push_back(std::pair(key, val));
-        return;
-    }
-    else
-    {
-        this->v[index].second = val;
-        return;
-    }
-    throw new std::runtime_error("put() failed");
-}
-int json_dict::find(json_object *key)
-{
-    int index = 0;
-    for (auto it = this->v.begin(); it != this->v.end(); it++)
-    {
-        if (it->first == key)
-            return index;
-        index++;
-    }
-    return -1;
-}
+
+// PUBLIC STATIC
--- week12/assignment_2/json_object.cpp
+++ week12/assignment_2/json_object.cpp
@@ -1,62 +1,12 @@
 #include "json_object.h"
-#include "data/String.h"
 
 int json_object::_index = 0;
 
 json_object *json_object::parse(const char *input, int length)
 {
-    // Initialize
-    json_object *obj;
-    int i = 0, j = 1;
-    bool sign = false;
-    // Loop
-    while (i < length)
-    {
-        sign = false;
-        switch (input[i])
-        {
-        case '{':
-            // DICT
-            break;
-        case '[':
-            // LIST
-            break;
-        case '\n':
-            // WHITESPACE
-            break;
-        case '\'':
-        case '\"':
-            // STRING
-            while (input[j] != input[i])
-                j++;
-            obj = String::parse((input + i), ((j - i) + 1), input[i]);
-            break;
-        case '-':
-            sign = true;
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            // INTEGER
-            break;
-        default:
-            throw new std::runtime_error("parse error");
-        }
-        // Iteration
-
-    }
-
-    // Return
-    return obj;
+    
 }
-
-json_object *json_object::parse(const std::string &str)
+json_object *json_object::parse(const std::string &input)
 {
-    return parse(str.c_str(), str.length());
+    return json_object::parse(input.c_str(), input.length());
 }
