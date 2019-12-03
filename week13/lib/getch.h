5ae4865977b7b8e7479d6098c16a97fc8b7cbb51
Ji Myoung Ha <webmaster@null0xff.com>
2019-12-03 10:36:29 +0900

    deleted week13

--- week13/lib/getch.h
+++
@@ -1,9 +0,0 @@
-#ifndef GETCH_H
-#define GETCH_H
-
-#include <stdio.h>
-#include <termio.h>
-
-int getch(void);
-
-#endif // GETCH_H
--- week13/object/object.cpp
+++
@@ -1,8 +0,0 @@
-#include "object.hpp"
-
-object::object() {}
-object::_type object::type() { return this->BLANK; }
-std::string object::to_string() const
-{
-    return _STRING_BLANK;
-}
